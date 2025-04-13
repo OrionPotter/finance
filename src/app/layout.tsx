a1a63bf5d009e5ae894bc6f053a8b96111979eee
liuzhi <1759586066@qq.com>
2025-04-13 17:27:15 +0800

    错误优化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,14 @@ import Navbar from '../components/Navbar';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
+       <head>
+        <meta charSet="utf-8" />
+        <title>零X金融</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
       <Navbar />
-      <body>{children}</body>
+      {children}</body>
     </html>
   );
 }
